639f05dc09249b665f5a998d44e7e16d899e9c11
imudin <imudin07@gmail.com>
2019-03-18 15:41:20 +0900

    version 1.1.0, height issue fixes

--- Panorama.vue
+++ Panorama.vue
@@ -78,7 +78,7 @@ export default {
         position: "relative",
         float: 'left',
         width: '100%',
-        height: '100%',
+        minHeight: '100px',
         backgroundSize: 'cover',
         cursor: 'move'
       };
